a93920d8fce1bf263736e2b5138faeb075ac5cde
jooyyoo <aliceyun9@naver.com>
2024-03-01 21:14:39 +0900

    [chore] #1 viewmodel 삭제

--- app/src/main/java/com/omg/cafebara/domain/usecase/SetDummyDataUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.omg.cafebara.domain.usecase
-
-import com.omg.cafebara.domain.repository.DummyDataRepository
-
-class SetDummyDataUseCase(
-    private val dummyDataRepository: DummyDataRepository
-) {
-    operator fun invoke(dummy: Int) = dummyDataRepository.setDummyData(dummy = dummy)
-}
