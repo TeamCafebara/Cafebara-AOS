64160b48e706ecd7eafec0a59703501fc5d94ad4
jooyyoo <aliceyun9@naver.com>
2024-03-14 23:46:07 +0900

    [feat] #3 EmployType 라이팅 추가

--- app/src/main/java/com/omg/cafebara/presentation/type/EmployType.kt
+++ app/src/main/java/com/omg/cafebara/presentation/type/EmployType.kt
@@ -1,5 +1,15 @@
 package com.omg.cafebara.presentation.type
 
-enum class EmployType {
-    EMPLOYER, EMPLOYEE
+import androidx.annotation.StringRes
+import com.omg.cafebara.R
+
+enum class EmployType(
+    @StringRes val titleRes: Int,
+) {
+    EMPLOYER(
+        R.string.name_view_input_name_employer
+    ),
+    EMPLOYEE(
+        R.string.name_view_input_name_employee
+    )
 }
