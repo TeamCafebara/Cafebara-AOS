6a8afc7637af762d799f9a9458b7494e9c579fb5
jooyyoo <aliceyun9@naver.com>
2024-03-15 12:47:29 +0900

    [feat] #3 BindingDialogFragment 생성

---
+++ app/src/main/java/com/omg/cafebara/util/base/BindingDialogFragment.kt
@@ -0,0 +1,42 @@
+package com.omg.cafebara.util.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+import com.omg.cafebara.databinding.DialogMiddleBinding
+
+abstract class BindingDialogFragment<B : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    DialogFragment() {
+    private var _binding: DialogMiddleBinding? = null
+    val binding get() = requireNotNull(_binding) { "${this::class.java.simpleName}에서 에러가 발생했습니다." }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DialogMiddleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+        )
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog?.setCancelable(false)
+        dialog?.setCanceledOnTouchOutside(true)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
