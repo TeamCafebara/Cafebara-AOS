1594d5cf38253d4a4c81bd00a7e7fe1c60367cf8
jooyyoo <aliceyun9@naver.com>
2024-03-21 01:33:24 +0900

    [chore] #7 ktlint 반영

--- app/src/main/java/com/omg/cafebara/presentation/employee/home/HomeFragment.kt
+++ app/src/main/java/com/omg/cafebara/presentation/employee/home/HomeFragment.kt
@@ -1,8 +1,12 @@
 package com.omg.cafebara.presentation.employee.home
 
+import android.os.Bundle
 import com.omg.cafebara.R
 import com.omg.cafebara.databinding.FragmentHomeBinding
 import com.omg.cafebara.util.base.BindingFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
--- app/src/main/java/com/omg/cafebara/presentation/employee/todo/TodoFragment.kt
+++ app/src/main/java/com/omg/cafebara/presentation/employee/todo/TodoFragment.kt
@@ -1,8 +1,12 @@
 package com.omg.cafebara.presentation.employee.todo
 
+import android.os.Bundle
 import com.omg.cafebara.R
 import com.omg.cafebara.databinding.FragmentTodoBinding
 import com.omg.cafebara.util.base.BindingFragment
 
 class TodoFragment : BindingFragment<FragmentTodoBinding>(R.layout.fragment_todo) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
