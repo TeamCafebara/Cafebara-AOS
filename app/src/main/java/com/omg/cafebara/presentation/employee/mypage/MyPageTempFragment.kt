921ed15a34b952f1bc994aa5f28bd09ff6249110
KIM MINJEONG <minjeong030420@gmail.com>
2024-04-02 01:57:33 +0900

    [ui] #9 마이페이지 UI 구현

---
+++ app/src/main/java/com/omg/cafebara/presentation/employee/mypage/MyPageTempFragment.kt
@@ -0,0 +1,15 @@
+package com.omg.cafebara.presentation.employee.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.omg.cafebara.R
+import com.omg.cafebara.databinding.FragmentMypageTempBinding
+import com.omg.cafebara.util.base.BindingFragment
+
+class MyPageTempFragment :
+    BindingFragment<FragmentMypageTempBinding>(R.layout.fragment_mypage_temp) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
