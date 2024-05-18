3fd8ef642d9f8f8f57df0cde940c6e912413ae12
emjayMJkim <minjeong030420@gmail.com>
2024-05-19 02:13:00 +0900

    [feat] #9 base dialog 생성

---
+++ app/src/main/java/com/omg/cafebara/util/base/BindingDialog.kt
@@ -0,0 +1,74 @@
+package com.omg.cafebara.util.base
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import androidx.viewbinding.ViewBinding
+import com.omg.cafebara.util.view.OriginalDialog
+
+abstract class BindingDialog: DialogFragment() {
+
+    protected var _binding: ViewBinding? = null
+    protected var title: String? = null
+    protected var content: String? = null
+    protected var contentVisible: Boolean? = null
+    protected var doBtnText: String? = null
+    protected var cancelBtnText: String? = null
+    protected lateinit var doBtnAction: () -> Unit
+    protected lateinit var cancelBtnAction: () -> Unit
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        setTitle()
+        setContent()
+        setContentVisible()
+        setDoBtnText()
+        setCancelBtnText()
+        setDoBtnClick { doBtnAction(); dismiss() }
+        setCancelBtnClick(doBtnAction)
+    }
+
+    abstract fun setTitle()
+    abstract fun setContent()
+    abstract fun setContentVisible()
+    abstract fun setDoBtnText()
+    abstract fun setCancelBtnText()
+    abstract fun setDoBtnClick(action: () -> Unit)
+    abstract fun setCancelBtnClick(action: () -> Unit)
+
+    class Builder() {
+        fun build(
+            title: String,
+            content: String? = null,
+            contentVisible: Boolean,
+            doBtnText: String,
+            cancelBtnText: String,
+            doBtnAction: () -> Unit,
+            cancelBtnAction: () -> Unit
+        ): BindingDialog {
+            val dialog = OriginalDialog()
+            return dialog.apply {
+                this.title = title
+                this.content = content
+                this.contentVisible = contentVisible
+                this.doBtnText = doBtnText
+                this.cancelBtnText = cancelBtnText
+                this.doBtnAction = doBtnAction
+                this.cancelBtnAction = cancelBtnAction
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    companion object {
+        const val DIALOG = "DIALOG"
+    }
+}
---
+++ app/src/main/java/com/omg/cafebara/util/view/OriginalDialog.kt
@@ -0,0 +1,58 @@
+package com.omg.cafebara.util.view
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.omg.cafebara.databinding.FragmentBaseDialogBinding
+import com.omg.cafebara.util.base.BindingDialog
+
+class OriginalDialog: BindingDialog() {
+
+    private val binding: FragmentBaseDialogBinding
+        get() = requireNotNull(_binding as FragmentBaseDialogBinding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBaseDialogBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return binding.root
+    }
+
+    override fun setTitle() {
+        binding.tvDialogBaseTitle.text = title
+    }
+
+    override fun setContent() {
+        binding.tvDialogBaseContent.text = content
+    }
+
+    override fun setContentVisible() {
+        if (contentVisible == true) {
+            binding.tvDialogBaseContent.visibility = View.VISIBLE
+        } else {
+            binding.tvDialogBaseContent.visibility = View.INVISIBLE
+        }
+    }
+
+    override fun setDoBtnText() {
+        binding.btnDialogBaseDo.text = doBtnText
+    }
+
+    override fun setCancelBtnText() {
+        binding.btnDialogBaseCancel.text = cancelBtnText
+    }
+
+    override fun setDoBtnClick(action: () -> Unit) {
+        binding.btnDialogBaseDo.setOnClickListener { action() }
+    }
+
+    override fun setCancelBtnClick(action: () -> Unit) {
+        binding.btnDialogBaseDo.setOnClickListener { action() }
+    }
+}
