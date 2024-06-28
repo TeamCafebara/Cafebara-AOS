03e5954c22332f55b844becb2a7b6b38f8caa537
jooyyoo <aliceyun9@naver.com>
2024-06-28 17:34:12 +0900

    [feat] #3 EmployType.kt edittext 힌트 속성 추가

--- app/src/main/java/com/omg/cafebara/presentation/type/EmployType.kt
+++ app/src/main/java/com/omg/cafebara/presentation/type/EmployType.kt
@@ -5,11 +5,18 @@ import com.omg.cafebara.R
 
 enum class EmployType(
     @StringRes val titleRes: Int,
+    @StringRes val hintRes: Int,
 ) {
     EMPLOYER(
-        R.string.name_view_input_name_employer
+        R.string.name_view_input_name_employer,
+        R.string.name_view_input_hint_employer
     ),
     EMPLOYEE(
-        R.string.name_view_input_name_employee
-    )
+        R.string.name_view_input_name_employee,
+        R.string.name_view_input_hint_employee
+    ),
+    EMPLOYER_GET_STORE_NAME(
+        R.string.name_view_input_store_name_employer,
+        R.string.store_name_view_input_hint
+    ),
 }
